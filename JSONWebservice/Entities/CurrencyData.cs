0c02d5969d6b55c8bc929e62c0b90b6209542e30
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2021-04-27 11:21:29 +0200

    Rename Currency to CurrencyData and add some classes and properties

---
+++ JSONWebservice/Entities/CurrencyData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class CurrencyData
+    {
+        #region Classes
+
+        public class Rates
+        {
+
+        }
+
+        public class Root
+        {
+
+            #region Private Fields
+
+            private byte[] _timestamp;
+            private string _base;
+            private Rates _rates;
+
+            #endregion
+
+            #region Public Properties
+
+            /// <summary>
+            /// What time the rates is from
+            /// </summary>
+            public byte[] Timestamp
+            {
+                get { return _timestamp; }
+                set { _timestamp = value; }
+            }
+
+            /// <summary>
+            /// What base currency is used
+            /// </summary>
+            public string Base
+            {
+                get { return _base; }
+                set { _base = value; }
+            }
+
+            /// <summary>
+            /// The different rates
+            /// </summary>
+            public Rates Rates
+            {
+                get { return _rates; }
+                set { _rates = value; }
+            }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
